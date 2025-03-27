1581ad656a9fb08830c47bd14cc7d8d85b3c4c14
Luis <langelgirona@gmail.com>
2025-03-27 10:12:27 -0600

    Actualizar clase Producto para manejar tallas y cantidades con un HashMap

--- Producto.java
+++ Producto.java
@@ -1,18 +1,36 @@
+import java.util.HashMap;
+
 class Producto {
     String sku;
     String nombre;
     String descripcion;
-    String tallas;
+    HashMap<String, Integer> tallasYCantidades;
 
     public Producto(String sku, String nombre, String descripcion, String tallas) {
         this.sku = sku;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.tallas = tallas;
+        this.tallasYCantidades = parseTallas(tallas);
+    }
+
+    private HashMap<String, Integer> parseTallas(String tallas) {
+        HashMap<String, Integer> map = new HashMap<>();
+        String[] parts = tallas.split(" ");
+        for (String part : parts) {
+            String[] tallaInfo = part.split("\\|");
+            if (tallaInfo.length == 2) {
+                try {
+                    map.put(tallaInfo[0], Integer.valueOf(tallaInfo[1]));
+                } catch (NumberFormatException e) {
+                    System.out.println("Error al convertir cantidad: " + part);
+                }
+            }
+        }
+        return map;
     }
 
     @Override
     public String toString() {
-        return "SKU: " + sku + ", Nombre: " + nombre + ", Descripción: " + descripcion + ", Tallas: " + tallas;
+        return "SKU: " + sku + ", Nombre: " + nombre + ", Descripción: " + descripcion + ", Tallas: " + tallasYCantidades;
     }
 }
